879e46ac2fdbd70b96cb7fc04ec30209a0d3a84d
nevillebradshaw <nevillebradshaw@hotmail.com>
2020-07-13 08:15:28 +0100

    refactor getGroup

--- src/njb/sudoku/Main.java
+++ src/njb/sudoku/Main.java
@@ -26,6 +26,7 @@ public class Main {
     };
 
     public static void main(String[] args) {
-        Solver solver = new Solver(GRID2);
+        Solver solver = new Solver(GRID);
+        solver = new Solver(GRID2);
     }
 }
--- src/njb/sudoku/SudokuGrid.java
+++ src/njb/sudoku/SudokuGrid.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 /**
  * @author nevillebradshaw@hotmail.com
- *
+ * <p>
  * This class represents the Grid of Cells
  * It provides methods to indicate if the Grid has been resolved
  * as well as the helper methods to List the nine values in a row,column or 3x3 group.
- * There are 9 3x3 groups in the grid which I refer to as GroupA, GroupB...GroupI
+ * There are 9 3x3 groups in the grid referred to as GroupA, GroupB...GroupI
  * These are the 3x3 arrays listed from top left of the grid to bottom right.
  */
 class SudokuGrid {
@@ -60,74 +60,48 @@ class SudokuGrid {
         List<Cell> groupCells = new ArrayList<>();
         // Group A
         if (row >= 0 && row <= 2 && col >= 0 && col <= 2) {
-            for (int i = 0; i <= 2; i++) {
-                for (int j = 0; j <= 2; j++) {
-                    groupCells.add(cellGrid_[i][j]);
-                }
-            }
+            groupCells = getGroup(0, 2, 0, 2);
         }
         // Group B
         else if (row >= 0 && row <= 2 && col >= 3 && col <= 5) {
-            for (int i = 0; i <= 2; i++) {
-                for (int j = 3; j <= 5; j++) {
-                    groupCells.add(cellGrid_[i][j]);
-                }
-            }
+            groupCells = getGroup(0, 2, 3, 5);
         }
         // Group C
         else if (row >= 0 && row <= 2 && col >= 6 && col <= 8) {
-            for (int i = 0; i <= 2; i++) {
-                for (int j = 6; j <= 8; j++) {
-                    groupCells.add(cellGrid_[i][j]);
-                }
-            }
+            groupCells = getGroup(0, 2, 6, 8);
         }
         // Group D
         else if (row >= 3 && row <= 5 && col >= 0 && col <= 2) {
-            for (int i = 3; i <= 5; i++) {
-                for (int j = 0; j <= 2; j++) {
-                    groupCells.add(cellGrid_[i][j]);
-                }
-            }
+            groupCells = getGroup(3, 5, 0, 2);
         }
         // Group E
         else if (row >= 3 && row <= 5 && col >= 3 && col <= 5) {
-            for (int i = 3; i <= 5; i++) {
-                for (int j = 3; j <= 5; j++) {
-                    groupCells.add(cellGrid_[i][j]);
-                }
-            }
+            groupCells = getGroup(3, 5, 3, 5);
         }
         // Group F
         else if (row >= 3 && row <= 5 && col >= 6 && col <= 8) {
-            for (int i = 3; i <= 5; i++) {
-                for (int j = 6; j <= 8; j++) {
-                    groupCells.add(cellGrid_[i][j]);
-                }
-            }
+            groupCells = getGroup(3, 5, 6, 8);
         }
         // Group G
         else if (row >= 6 && row <= 8 && col >= 0 && col <= 2) {
-            for (int i = 6; i <= 8; i++) {
-                for (int j = 0; j <= 2; j++) {
-                    groupCells.add(cellGrid_[i][j]);
-                }
-            }
+            groupCells = getGroup(6, 8, 0, 2);
         }
         // Group H
         else if (row >= 6 && row <= 8 && col >= 3 && col <= 5) {
-            for (int i = 6; i <= 8; i++) {
-                for (int j = 3; j <= 5; j++) {
-                    groupCells.add(cellGrid_[i][j]);
-                }
-            }
+            groupCells = getGroup(6, 8, 3, 5);
         }
         // Group I
         else if (row >= 6 && row <= 8 && col >= 6 && col <= 8) {
-            for (int i = 6; i <= 8; i++) {
-                for (int j = 6; j <= 8; j++) {
-                    groupCells.add(cellGrid_[i][j]);
-                }
+            groupCells = getGroup(6, 8, 6, 8);
+        }
+        return groupCells;
+    }
+
+    private List<Cell> getGroup(int rowMin, int rowMax, int colMin, int colMax) {
+        List<Cell> groupCells = new ArrayList<>();
+        for (int i = rowMin; i <= rowMax; i++) {
+            for (int j = colMin; j <= colMax; j++) {
+                groupCells.add(cellGrid_[i][j]);
             }
         }
         return groupCells;
